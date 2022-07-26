d87e0355c7501aabaf1d0336e29297ea53bb88d5
Artur Finger <artur.finger@nangu.tv>
2022-07-26 19:10:40 +0300

    Added a script to spawn a new TS library project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # spawn-tslib
-A handly utility to spawn a TS library ready to go. All the annoying webpack, jest, tsconfig event linting setup is done automatically for you.
+A handy utility to spawn a TS library ready to go.
+
+All the annoying webpack, jest and tsconfig setup is done automatically for you, and you can go ahead and start developing your library today!
+
+Simply create a new directory for you lib and inside run
+
+```
+npx @finga/spawn-tslib
+```
+
+*Your directory must be empty. The only thing it can contain is a .git index.*
+
+## What gets set up for you
+- Typescript and tsconfig.json
+- Webpack configured to transpile to ES5
+- Jest for unit testing
+- Eslint with reasonable basic rules
+- Installs dependencies via NPM
+
+## Alternatives
+If you are not fully satisfied with `@finga/spawn-tslib`, go and check out https://github.com/ryancat/create-typescript-library,
+it is a good looking alternative with the same purpose and it is more configurable.
---
+++ index.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const fse = require('fs-extra')
+const child = require('child_process')
+const chalk = require('chalk')
+
+const templateDir = path.resolve(__dirname, 'template')
+const targetDir = process.cwd()
+
+/**
+ * @returns true if directory is empty, or if it has a single .git folder inside and otherwise it is empty
+ */
+const isEmptyDir = (dir) => {
+  const files = fse.readdirSync(dir)
+
+  if (files.length === 0) {
+    return true
+  }
+
+  if (files.length === 1) {
+    return files[0] === '.git'
+  }
+
+  return false
+}
+
+if (!isEmptyDir(targetDir)) {
+  console.error(chalk.red(`Target directory ${targetDir} is not empty. Make sure it is empty or contains only .gitignore file.`))
+  process.exit(1)
+}
+
+console.info('Spawning a TS library')
+console.info(chalk.yellow(` - Setting up files and configs [1/2]...`))
+
+// console.info(`Debug: Copying files ${templateDir} -> ${targetDir}`)
+
+try {
+  fse.copySync(templateDir, targetDir, { recursive: true })
+} catch (error) {
+  console.error(chalk.red(`\nFailed to copy config files to ${targetDir}`), error)
+  process.exit(1)
+}
+
+console.info(chalk.yellow(` - Installing dependencies [2/2]...\n`))
+
+try {
+  child.execSync('npm i', { stdio: 'inherit' })
+} catch (error) {
+  console.error(chalk.red(`\nFailed to install dependencies via "npm i"`))
+  process.exit(1)
+}
+
+console.log(chalk.green('\nSuccess!'))
+process.exit(0)
